1e9d8c62b6f6c2533174f1783ba0463dfa31671b
lispincsclothing <scullingnull+lisp@gmail.com>
2016-04-07 12:12:34 -0700

    add /todo & todo/:id routes

--- server.js
+++ server.js
@@ -24,6 +24,23 @@ app.get('/todos', function(request, response) {
   response.json(todos);
 });
 
+app.get('/todos/:id', function(request, response) {
+
+  var input_id = parseInt(request.params.id,10);
+  var result = null;
+  todos.forEach(function(value, index) {
+    // console.log(value.id +"  "+input_id);
+    if (value.id === input_id){
+       result = value;
+    }
+  });
+  if (result) {
+    response.json(result);
+  } else {
+    response.status(404).send();
+  }
+});
+
 
 app.listen(PORT, function() {
   console.log('Express listening on port ' + PORT );

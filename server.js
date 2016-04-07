5d61154a1892606b56017763d53560115d52ae69
lispincsclothing <scullingnull+lisp@gmail.com>
2016-04-07 11:23:51 -0700

    All todos JSON

--- server.js
+++ server.js
@@ -2,9 +2,28 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+  id:1,
+  description: 'go do something',
+  completed: false
+},{
+  id:2,
+  description: 'go do something else',
+  completed: false
+},{
+  id:3,
+  description: 'go do something completely different',
+  completed: true
+}];
+
 app.get('/', function(request, response) {
   response.send('ToDo App is launched! on Root');
 });
+
+app.get('/todos', function(request, response) {
+  response.json(todos);
+});
+
 
 app.listen(PORT, function() {
   console.log('Express listening on port ' + PORT );

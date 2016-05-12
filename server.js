2899ee28cc1075e6b358576bede569ace2cbbf22
[A <scullingnull+lisp@gmail.com>
2016-05-12 14:21:45 -0700

    Update PUT /todos/:id to use sequlize

--- server.js
+++ server.js
@@ -20,23 +20,25 @@ app.get('/todos', function(request, response) {
     var where = {};
 
     if (query.hasOwnProperty('completed') && query.completed === 'true') {
-      where.completed = true;
+        where.completed = true;
     } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
-      where.completed = false;
+        where.completed = false;
     }
 
     if (query.hasOwnProperty('word') && query.word.length > 0) {
-          where.description = {
-              $like: '%' + query.word + '%'
-          }
+        where.description = {
+            $like: '%' + query.word + '%'
+        }
     }
 
-    db.todo.findAll({where: where})
-    .then(function (todos) {
-      response.json(todos);
-    }, function (e) {
-      response.status(500).send();
-    })
+    db.todo.findAll({
+            where: where
+        })
+        .then(function(todos) {
+            response.json(todos);
+        }, function(e) {
+            response.status(500).send();
+        })
 });
 
 app.get('/todos/:id', function(request, response) {
@@ -44,15 +46,15 @@ app.get('/todos/:id', function(request, response) {
     var input_id = parseInt(request.params.id, 10);
 
     db.todo.findById(input_id)
-    .then(function(todo) {
-        if (!!todo) {  //converting to truth version (using !!) because todo will be null or object - looking for null
-            response.json(todo.toJSON());
-        } else {
-            response.status(404).send();
-        }
-    }, function(e) {
-        response.status(500).send(e);
-    });
+        .then(function(todo) {
+            if (!!todo) { //converting to truth version (using !!) because todo will be null or object - looking for null
+                response.json(todo.toJSON());
+            } else {
+                response.status(404).send();
+            }
+        }, function(e) {
+            response.status(500).send(e);
+        });
 });
 
 app.post('/todos', function(request, response) {
@@ -71,50 +73,59 @@ app.delete('/todos/:id', function(request, response) {
     var input_id = parseInt(request.params.id, 10);
 
     db.todo.destroy({
-        where: {
-          id: input_id
-        }
-    })
-    .then(function(rowsDeleted) {
-      if (rowsDeleted === 0) {
-        response.status(404).json({
-          error: 'No todo with id'
+            where: {
+                id: input_id
+            }
+        })
+        .then(function(rowsDeleted) {
+            if (rowsDeleted === 0) {
+                response.status(404).json({
+                    error: 'No todo with id'
+                });
+            } else {
+                response.status(204).send();
+            }
+        }, function(e) {
+            response.status(500).send(e);
         });
-      } else {
-        response.status(204).send();
-      }
-    }, function(e) {
-    response.status(500).send(e);
-    });
 });
 
 app.put('/todos/:id', function(request, response) {
     var input_id = parseInt(request.params.id, 10);
-    var result = _.findWhere(todos, {
-        id: input_id
-    });
-    if (!result) {
-        return response.status(404).send();
-    }
+    // var result = _.findWhere(todos, {
+    //     id: input_id
+    // });
+    // if (!result) {
+    //     return response.status(404).send();
+    // }
 
     var body = _.pick(request.body, 'completed', 'description');
-    var validAttributes = {};
+    var attributes = {};
 
-    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-        validAttributes.completed = body.completed;
-    } else if (body.hasOwnProperty('completed')) {
-        return response.status(400).send();
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
     }
 
-    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-        validAttributes.description = body.description;
-    } else if (body.hasOwnProperty('description')) {
-        return response.status(400).send();
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
     }
 
-    _.extend(result, validAttributes);
-    response.json(result);
-});
+    db.todo.findById(input_id).then(function(todo) {
+        if (todo) {
+            todo.update(attributes).then(function(todo) {
+              response.json(todo.toJSON());
+            },
+            function(e) {
+              response.status(400).json(e);
+            });
+        } else {
+            response.status(404).send();
+        }
+        },
+      function() {
+          response.status(500).send();
+      });
+  });
 
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
